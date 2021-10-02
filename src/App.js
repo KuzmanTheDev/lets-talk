4bea9bf07cd2bd4a673b4099800e41429e73f10d
HickeyMonster <kuzmanium@gmail.com>
2021-10-02 10:41:19 +0100

    fix deploy issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import Admin from "./screens/Admin/Admin";
 import Dashboard from "./screens/Dashboard/index";
